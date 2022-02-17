7eb3e5ac76fb884e001e81538c9e48b454034c22
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-17 14:07:00 +0800

    Initial setup

---
+++ api/index.js
@@ -0,0 +1,37 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const startApolloServer = async (app, httpServer) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
